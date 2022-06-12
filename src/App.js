537b51b048034c7b2c676feeb0aa3a640ba3ba56
jadensiow <jianhong93@gmail.com>
2022-06-12 16:29:39 +0800

    Update page with tabs

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import FirstFunction from "./components/FirstFunction";
 import SecondFunction from "./components/SecondFunction";
 
 const App = () => {
+  const [tabNumber, setTabNumber] = useState(1);
+
   return (
     <div className="App">
-      <SecondFunction />;
+      <div className="col text-center">
+        <h2>Technical Assignment</h2>
+
+        <button onClick={() => setTabNumber(1)}>Function 1</button>
+        <button onClick={() => setTabNumber(2)}>Function 2</button>
+
+        <div>{tabNumber === 1 ? <FirstFunction /> : <SecondFunction />}</div>
+      </div>{" "}
     </div>
   );
 };
